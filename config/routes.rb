7ed1cc55058b5553b260542fb36d0fe736a64858
SangCoder <dinhsang111997@gmail.com>
2022-04-14 11:31:31 +0700

    error router sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
-Rails.application.routes.draw do
-  resources :courses
-  root 'home#index'
-  # get 'static_pages/landing_page'
-  # get 'static_pages/privacy_policy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   devise_for :users
+#   resources :courses
+#   root 'home#index'
+#   # get 'static_pages/landing_page'
+#   # get 'static_pages/privacy_policy'
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+# end
+Rails.application.routes.draw do
+  devise_for :users
+  resources :courses
+  get 'home/index'
+  root 'home#index'
 end
